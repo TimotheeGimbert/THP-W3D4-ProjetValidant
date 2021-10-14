8bda8809b9abb38605df069c883384a0e73c21c3
TimotheeGimbert <timothee.gimbert@free.fr>
2021-10-14 23:03:57 +0200

    solves exo bonus

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,21 +1,26 @@
 require 'open-uri'
 require 'nokogiri'
 
-def get_depute_info(url) # returns a hash { "first_name" => "Jean", "last_name" ... }
+def get_depute_info(url) # returns a hash { "name" => "Jean GEINON" ... }
   depute_page = Nokogiri::HTML(URI.open(url))
   name = get_depute_name(depute_page) 
   email = get_depute_email(depute_page)
+  depute_hash = { "name" => name, "email" => email }
+  puts depute_hash
+  puts
+  return depute_hash
 end
 
 def get_depute_name(depute_page)
   name = depute_page.xpath('//article/div[2]/h1').text
-  puts name
+  print name
   return name
 end
 
 def get_depute_email(depute_page)
   email = depute_page.xpath('//*[@class="deputes-liste-attributs"]/dd[4]//li[2]/a').text
-  puts email
+  puts " : " + email
+  return email
 end
 
 def get_deputes_urls() # returns an ARRAY of urls to scrap
@@ -27,8 +32,9 @@ end
 
 
 def perform
+  system('clear')
   a = Array.new
-  deputes_urls = get_deputes_urls() # we generate an array with the depute's pages urls
+  deputes_urls = get_deputes_urls() 
   deputes_urls.each { |url| a.push(get_depute_info(url)) }
   puts a
 end
