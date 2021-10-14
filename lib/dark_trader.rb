43f7549bb6ebab4b325e30677ec20c3b51610147
TimotheeGimbert <timothee.gimbert@free.fr>
2021-10-14 21:05:43 +0200

    fixes dark_floats and adds testings

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -9,12 +9,12 @@ end
 
 def get_coins_prices(page) # returns an ARRAY of prices as STRINGS
   prices_array = page.xpath('//tbody//tr/td[5]/div/a').to_a
-  prices_array.map! {|node| node = node.text[1..-1]} # makes nodes as astring and remove $ sign
+  prices_array.map! {|node| node = node.text.delete(",")[1..-1].to_f} # makes nodes as astring and remove $ sign
   return prices_array
 end
 
 
-def crypto_scrapper(page) # returns an ARRAY of coins data as HASHES
+def crypto_scraper(page) # returns an ARRAY of coins data as HASHES
   tokens = Array.new # array that will hold final data
 
   names_array = get_coins_names(page)
@@ -30,7 +30,7 @@ end
 
 def perform
   page = Nokogiri::HTML(URI.open('https://coinmarketcap.com/all/views/all/'))
-  puts crypto_scrapper(page)
+  puts crypto_scraper(page)
 end
 
 perform
--- spec/dark_trader_spec.rb
+++ spec/dark_trader_spec.rb
@@ -1,13 +1,27 @@
 require_relative '../lib/dark_trader.rb'
 
-describe "perform method" do
-  it "should return an array" do
-    expect(perform).to be_a(Array)
-  end
-  it "should return an array of hashes" do
-    expect(perform[0]).to be_a(Hash)
-  end
-  it "should return an array of at least 15 coins" do
-    perform().length.should >= 15
+page = Nokogiri::HTML(URI.open('https://coinmarketcap.com/all/views/all/'))
+ 
+describe "get_coins_names method" do
+  it "should return an array of strings" do
+    expect(get_coins_names(page)).to be_a(Array)
+    expect(get_coins_names(page)[0]).to be_a(String)
+    expect(get_coins_names(page)[0]).to be == "BTC"
   end
 end
+
+describe "get_coins_prices method" do
+  it "should return an array of floats" do
+    expect(get_coins_prices(page)).to be_a(Array)
+    expect(get_coins_prices(page)[0]).to be_a(Float)
+  end
+end
+
+describe "crypto_scraper method" do
+  it "should return an array of hashes" do
+    expect(crypto_scraper(page)).to be_a(Array)
+    expect(crypto_scraper(page)[0]).to be_a(Hash)
+  end
+end
+
+
